d99ef085846d6909f4e0e535d2c25afeeb42fb66
magnusbutlin <magnusbutlin2@hotmail.com>
2016-11-09 17:50:18 +1100

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -28,14 +28,14 @@ if [[ $TRAVIS_BRANCH == '8.x' && $TRAVIS_REPO_SLUG == "magnusbutlin/grails-tomca
    if [[ $filename != *-SNAPSHOT* ]]
    then
      git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b 8.x master --single-branch > /dev/null
-     cd gh-pages
+     cd master
      git rm -rf .
      cp -r ../target/docs/. ./
      git add *
      git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
      git push origin HEAD
      cd ..
-     rm -rf gh-pages
+     rm -rf master
    else
      echo "SNAPSHOT version, not publishing docs"
    fi
